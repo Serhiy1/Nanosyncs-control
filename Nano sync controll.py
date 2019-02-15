8e59a12940c6e8ef1e34e89e3d2b381700ab8efc
Serhiy1 <serhiy1@live.co.uk>
2019-02-15 14:29:32 +0000

    update nano sync control.py

--- Nano sync controll.py
+++ Nano sync controll.py
@@ -18,7 +18,7 @@ class NanoSync:
         self.cursor_pos = 0              # byte 0: current cursor position  [placeholder]
         self.video_ref = 1               # byte 1: INTERNAL = 1, EXT PAL = 2 , EXT NTSC = 3 , EXT.TRI = 4
         self.video_standard = 1          # byte 2: NTSC = 1, PAL 25 =2, PAL 24 = 3, PAL 23.98= 4
-        self.HD_standard = 1             # byte 3: 1080i = 1, 1080p = 2, 1080p x 2fps = 3, 720p = 4, 720p x 2fps = 5
+        self.HD_standard = 1             # byte 3: 1080i x 2fps = 1, 1080p = 2, 1080p x 2fps = 3, 720p = 4, 720p x 2fps = 5
         self.FPS = 1                     # byte 4: 23.98fps = 1, 24fps = 2, 25fps = 3, 29.97fps = 4, 30fps = 5
         self.video_1_to_3 = 1            # byte 5: SD = 1, HD = 2
         self.video_4 = 2                 # byte 6: SD = 1, HD = 2
@@ -39,9 +39,9 @@ class NanoSync:
         self.SPDIF_multiplier = 1        # byte 19: x1 = 1, x2 = 2
 
         # important commands used for nan
-        self.serial_message = [240, 44, 78, 83, 1, 247]  # F0 2C 4E 53 01 F7 - command to return S/N of Nano Sync
-        self.query_current_Config = [240, 44, 78, 83, 3, 247]  # F0 2C 4E 53 03 F7 - Queries the nano sync about its current setup
-        self.write_new_config = []  # F0 2C 4E 53 0F [20 data bytes] F7
+        self.serial_message = [240, 44, 78, 83, 1, 247]         # F0 2C 4E 53 01 F7 - command to return S/N of Nano Sync
+        self.query_current_Config = [240, 44, 78, 83, 3, 247]   # F0 2C 4E 53 03 F7 - Queries the nano sync about its current setup
+        self.write_new_config = []                              # F0 2C 4E 53 0F [20 data bytes] F7
 
         self.video_config = []  # containers for the video variables above, should make it easier to edit
         self.audio_config = []  # containers for the audio variables above, should make it easier to edit
@@ -89,7 +89,7 @@ class NanoSync:
             self.firmware_version = self.firmware_version[:2] + '.' + self.firmware_version[2:]
 
             print("connected to NanoSync - serial number: " + self.serial_number + "\n"
-                  "Firmware version " + self.firmware_version)
+                                                                                   "Firmware version " + self.firmware_version)
 
     def select_correct_port(self, midi_list):
 
@@ -102,7 +102,7 @@ class NanoSync:
                     print("found ")
                     return i
         except IOError:
-            error =("unable to find nano sync")
+            error = "unable to find nano sync"
             return error
 
     def send_message(self, message):
@@ -158,7 +158,8 @@ class NanoSync:
 
     def update_current_config(self):
 
-        self.video_config = [self.cursor_pos, self.video_ref, self.video_standard, self.HD_standard, self.FPS, self.video_1_to_3,
+        self.video_config = [self.cursor_pos, self.video_ref, self.video_standard, self.HD_standard, self.FPS,
+                             self.video_1_to_3,
                              self.video_4, self.video_5, self.video_6]
         self.audio_config = [self.audio_ref, self.external_word_fs, self.external_word_fs_x2, self.external_word_fs_1,
                              self.external_LTC_fps, self.audio_sample_rate, self.sample_rate_P_factor,
@@ -178,7 +179,7 @@ class NanoSync:
         for i in range(5):
             self.send_message(self.new_command())
             self.get_current_config()
-            if comparision != self.current_config:
+            if comparision != self.current_config:  # This may provide a false positive if the nanosync is pre-configured with the same setting being sent
                 print("successfully sent command")
                 break
         else:
@@ -200,7 +201,7 @@ class NanoSync:
         byte_0 = ["Placeholder"]
         byte_1 = ["INTERNAL", "EXT PAL", "EXT NTSC", "EXT TRI LVL"]
         byte_2 = ["SD standard", "NTSC", "PAL 25", "PAL 24", "PAL 23.98"]
-        byte_3 = ["1080i", "1080p", "1080p x 2fps", "720p", "720p x 2fps"]
+        byte_3 = ["1080i x 2fps", "1080p", "1080p x 2fps", "720p", "720p x 2fps"]
         byte_4 = ["23.98fps", "24fps", "25fps", "29.97fps", "30fps"]
         byte_5 = ["SD", "HD"]
         byte_6 = ["SD", "HD"]
@@ -209,19 +210,20 @@ class NanoSync:
         byte_9 = ["follow video", "external word clock", "external word 1:1", "LTC"]
         byte_10 = ["44.1Khz", "48Khz"]
         byte_11 = ["x1", "x2"]
-        byte_12 = ["1/1",   "+0.1%"]
+        byte_12 = ["1/1", "+0.1%"]
         byte_13 = ["23.98 fps", " 24fps", "25fps", "29.97fps", "30fps"]
         byte_14 = ["48Khz", "44.1Khz"]
         byte_15 = ["x1", "+4%", "+0.1%", "-0.1%", "-4%"]
-        byte_16 = ["x1", "x2",  "x4"]
-        byte_17 = ["x1", "x2",  "x4", "x256"]
+        byte_16 = ["x1", "x2", "x4"]
+        byte_17 = ["x1", "x2", "x4", "x256"]
         byte_18 = ["x1", "x2"]
         byte_19 = ["x1", "x2"]
 
         byte_list = [byte_0, byte_1, byte_2, byte_3, byte_4, byte_5, byte_6, byte_7, byte_8, byte_9, byte_10, byte_11,
                      byte_12, byte_13, byte_14, byte_15, byte_16, byte_17, byte_18, byte_19]
 
-        byte_names_list = ["cursor_pos", "video_ref = ", "video_standard = ", "HD_standard = ", "FPS = ", "video 1 - 3 = ",
+        byte_names_list = ["cursor_pos", "video_ref = ", "video_standard = ", "HD_standard = ", "FPS = ",
+                           "video 1 - 3 = ",
                            "video 4 = ", "video 5 = ", "video 6 = ", "audio reference = ", "external word fs = ",
                            "external_word fs x2 = ", "external_word fs 1 = ", "external LTC fps = ",
                            "audio sample rate = ", "sample rate Pull factor = ", "word multiplier 1 to 6 = ",
@@ -233,22 +235,12 @@ class NanoSync:
                 i += 1
                 pass
             else:
-                print(byte_names_list[i], byte_list[i][element-1])
+                # print(byte_names_list[i], byte_list[i][element - 1])
+                log_string = str(byte_names_list[i]) + str(byte_list[i][element - 1])
+                print(log_string)
                 i += 1
 
-    def test_function(self):
-        test_file = open("test.txt", "r")
-        test_command = test_file.readline()
+    def close(self):
+        self.nanosync_midi_out.close_port()
+        self.nanosync_midi_in.close_port()
 
-        while True:
-            if test_command == "END\n":
-                break
-            else:
-                test_command = test_command[:-1]
-                test_command = test_command.split(",")
-
-                for i in range(len(test_command)):
-                    test_command[i] = int(test_command[i])
-
-                self.send_new_config(test_command)
-                test_command = test_file.readline()
